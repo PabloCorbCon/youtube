e62107c046af04c9647c89a3931b8a601b5c8570
Pablo Corbalán <pablo.corbalan@protonmail.com>
2021-03-10 17:34:57 +0100

    Update hello.asm

--- es/hello-world-en-assembly/hello.asm
+++ es/hello-world-en-assembly/hello.asm
@@ -2,23 +2,20 @@
 ; x86 asm por Pablo Corbalán para su canal de YouTube. Por favor lee el 
 ; archivo README.md de esta carpeta para más información, y si vas a 
 ; copiar el código deja créditos o por lo menos un like en el canal de yt :)
+section        .text                   ; declare the .text section
+global         _start                  ; has to be declared for the linker (ld)
+_start:                                ; entry point for _start
+    mov edx, len                       ; "invoke" the len of the message
+    mov ecx, msg                       ; "invoke" the message itself
 
-section       .text 
+    mov ebx, 1                         ; set the file descriptor (fd) to stdout
 
-global        _start                    ; tenemos que declararla para ld
+    mov eax, 4                         ; system call for "write"   
+    int 0x80                           ; call the kernel
 
-_start:
-  mov         edx, len                  ; llamamos a la longitud, almacenada en len
-  mov         ecx, msg                  ; llamamos al mensaje, almacenado en msg
-  mov         ebx, 1                    ; "creamos" (set) el descriptor (stdout)
+    mov eax, 1                         ; system call for "exit"
+    int 0x80                           ; call the kernel
 
-  mov         eax, 4                    ; llamamos al sistema para escribir
-  int         0x80                      ; llamamos al kernel
-
-  mov         eax, 1                    ; llamamos al sistema para salir
-  int         0x80                      ; volvemos a llamar al kernel
-
-section       .data                     ; aqui declaramos datos y variables
-  msg         db "Hello world", 0xa     ; el mensaje que vamos a imprimir
-  len         equ $ -msg                ; la longitud del mensaje
-
+section        .data                   ; here you declare the data
+    msg        db "Hello world!"       ; the actual message to use
+    len        equ $ -msg              ; get the size of the message
